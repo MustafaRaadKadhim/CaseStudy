a4711d0939ff4c65d196f55e557f0711eab1e2b2
Kadhim Mustafa Raad Kadhim <kadhim_mustafa@uestc.edu.cn>
2024-07-05 13:57:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -166,22 +166,3 @@ elif Validation == "Feature and Distance Validation":
             print("Feature and Distance Validation: -- C#", i, " out of ", count, " -- Results: \n",np.round(ResultsArray[i, :].tolist(), 4).tolist(), "\n")
         filename = "Features_Distance_Validation_F_" + str(f_idx) + ".pkl"
         pickle_.Saver(os.path.join(save_path,filename), ResultsArray)
-
-        
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load
-
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
-
-import os
-for dirname, _, filenames in os.walk('/kaggle/input'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
-
-# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
-# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
